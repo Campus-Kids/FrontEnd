0d77573bb553517a072ecc81d53bb6abf9ca888c
IvanHernandezPelaez <ivan_hernandez09@hotmail.com>
2024-04-02 22:57:43 -0500

    Resgistro de producto OK con el Back

--- campuskids/src/Componentes/Productos/Productos.jsx
+++ campuskids/src/Componentes/Productos/Productos.jsx
@@ -1,13 +1,79 @@
-import React from "react";
-
 import { PhotoIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Footer from "../Footer/Footer";
+import React, { useEffect, useState } from "react";
+import { fetchObtenerCategorias } from "../../Servicio/Api";
+import { fetchRegistrarProducto } from "../../Servicio/Api";
 
 const Productos = () => {
+  const [nombreProducto, setNombreProducto] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [categorias, setCategorias] = useState([]);
+  const [categoriaEnviar, setCategoriaEnviar] = useState(null);
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState("");
+  const [imagenes, setImagenes] = useState([]);
+
+  useEffect(() => {
+    fetchObtenerCategorias()
+      .then((data) => {        
+        setCategorias(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleNombreChange = (e) => {
+    setNombreProducto(e.target.value);
+  };
+
+  const handleDescripcionChange = (e) => {
+    setDescripcion(e.target.value);
+  };
+
+  const handleCategoriaChange = (e) => {    
+    const selectedCategoriaId = parseInt(e.target.value, 10);
+    console.log("ID de la categoría seleccionada:", selectedCategoriaId);
+    const selectedCategoria = categorias.find(
+      (categoria) => categoria.id === selectedCategoriaId
+    );
+    console.log("Categoría seleccionada:", selectedCategoria);
+    // setCategoriaEnviar(selectedCategoria); //ELIMINAR ESTO PARA PRIBAR
+    setCategoriaSeleccionada(selectedCategoriaId);
+  };
+
+  const handleImagenesChange = (e) => {
+    // Aquí debes manejar la carga de imágenes y actualizar el estado de 'imagenes'
+    const files = e.target.files;
+    const selectedImages = [];
+    for (let i = 0; i < files.length && i < 5; i++) {
+      const url = URL.createObjectURL(files[i]);
+      selectedImages.push({ url });
+    }
+    setImagenes(selectedImages);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const idCategoria = parseInt(categoriaSeleccionada, 10);
+      const productoResponse = await fetchRegistrarProducto({
+        nombre: nombreProducto,
+        descripcion: descripcion,
+        idCategoria: categoriaSeleccionada, //categoriaEnviar.id, 
+        imagenes: imagenes.map((imagen) => ({ url: imagen.url })),
+      });
+      console.log("Respuesta del servidor:", productoResponse);
+    } catch (error) {
+      console.error("Error al enviar los datos al servidor:", error);
+    }
+    
+  };
+
   return (
     <div>
       <div className="max-w-lg mx-auto border border-green-500 p-6 rounded-lg bg-gray-100">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="space-y-12">
             {/* AQUÍ EMPIEZA EL FORMULARIO */}
 
@@ -27,6 +93,7 @@ const Productos = () => {
                 id="first-name"
                 autoComplete="given-name"
                 className="block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                onChange={(e) => setNombreProducto(e.target.value)}
               />
             </div>
 
@@ -44,10 +111,12 @@ const Productos = () => {
                 name="country"
                 autoComplete="country-name"
                 className="block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                onChange={handleCategoriaChange}
               >
-                <option>United States</option>
-                <option>Canada</option>
-                <option>Mexico</option>
+                <option value="">Seleccione una categoría</option>
+                {categorias.map((categoria) => (
+                  <option key={categoria.id} value={categoria.id} > {categoria.nombre}</option>
+                ))}
               </select>
             </div>
 
@@ -60,12 +129,13 @@ const Productos = () => {
               Descripción
             </label>
             <div className="mt-2">
-              <textarea
-                id="about"
-                name="about"
+              <textarea                
+                id="descripcion"
+                name="descripcion"
+                value={descripcion}
+                onChange={(e) => setDescripcion(e.target.value)}
                 rows={3}
                 className="block w-full rounded-md border-1 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                defaultValue={""}
               />
             </div>
 
@@ -88,11 +158,14 @@ const Productos = () => {
                     className="relative cursor-pointer rounded-md bg-white font-semibold text-[#385bbb] focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                   >
                     <span>Upload a file</span>
-                    <input
+                    <input                      
                       id="file-upload"
                       name="file-upload"
                       type="file"
                       className="sr-only"
+                      accept="image/*"
+                      multiple
+                      onChange={handleImagenesChange}
                     />
                   </label>
                   <p className="pl-1">or drag and drop</p>
@@ -120,9 +193,10 @@ const Productos = () => {
           </div>
         </form>
       </div>
-      <Footer />
+      <Footer/>
     </div>
   );
 };
 
 export default Productos;
+
--- campuskids/src/Servicio/Api.js
+++ campuskids/src/Servicio/Api.js
@@ -1,26 +1,26 @@
 const urlBase = "http://localhost:8080";
 
-// export const fetchRegistrarProducto = async (data) => {
+export const fetchRegistrarProducto = async (data) => {
     
-//     console.log("datos desde el front", data);
+    console.log("datos desde el front", data);
     
-//     const url = `${urlBase}/producto`;
+    const url = `${urlBase}/producto`;
 
-//     const response = await fetch(url, {
-//         method: "POST",
+    const response = await fetch(url, {
+        method: "POST",
     
-//         headers: {
-//           "Content-Type": "application/json"          
-//         },
-//         body: JSON.stringify(data),
-//       });
+        headers: {
+          "Content-Type": "application/json"          
+        },
+        body: JSON.stringify(data),
+      });
 
-//       if (!response.ok) {
-//         return response.status;
-//       }
-//       return await response.text();
+      if (!response.ok) {
+        return response.status;
+      }
+      return await response.json();
     
-// };
+};
 
 
 export const fetchObtenerProducto = async () => {
@@ -41,15 +41,21 @@ export const fetchObtenerProducto = async () => {
     
 };
 
-// export const fetchListarProductos = async () => {
-//     const url = ${baseUrl}/producto/productosAll;
-    
-//     const response = await fetch(url);
-    
-    
-//     console.log("Productos: ", response);
-//     if (!response.ok) {
-//       throw new Error("Error en la solicitud: " + response.status);
-//     }
-//     return await response.json();
-//   };
+export const fetchObtenerCategorias = async () => {
+  const url = `${urlBase}/categoria`;          
+
+  const response = await fetch(url, {
+      method: "GET",    
+      headers: {
+        "Content-Type": "application/json"          
+      }
+      
+    });
+
+    if (!response.ok) {
+      return response.status;
+    }
+    return await response.json();
+  
+};
+
